049973937cd929a535c0f7ae55a8bcd24b29b5bd
gys <1094317710@qq.com>
2017-05-24 21:23:49 +0800

    1111

---
+++ Koujue.java
@@ -0,0 +1,16 @@
+public class Koujue
+{
+ public static void main(String[] args)
+ {
+  for(int i=1;i<=9;i++)
+  {
+   for(int j=1;j<=9;j++)
+   {
+    System.out.print(j+"*"+i+"="+i*j+"\t");
+    if(i==j)
+     break;
+   }
+   System.out.println();
+  }
+ }
+} 
